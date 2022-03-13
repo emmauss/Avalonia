f26ea092f850ea759df24403e66aaa6b73ac96f3
Emmanuel Hansen <emmausssss@gmail.com>
2022-03-13 13:20:20 +0000

    removed conflict marker

--- samples/ControlCatalog.Android/SplashActivity.cs
+++ samples/ControlCatalog.Android/SplashActivity.cs
@@ -1,13 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.OS;
-<<<<<<< HEAD
-=======
-using Application = Android.App.Application;
-
-using Avalonia;
-using Avalonia.Android;
->>>>>>> 0f6e65aa4 (android support)
 
 namespace ControlCatalog.Android
 {
@@ -22,7 +15,7 @@ namespace ControlCatalog.Android
         protected override void OnResume()
         {
             base.OnResume();
-            
+
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
